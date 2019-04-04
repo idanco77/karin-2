cc8aeab52110aa23e2fd2e3bf7351f277bee5d0d
Idan Cohen <idanco77@gmail.com>
2019-04-04 16:47:58 +0300

    init commit

---
+++ index.php
@@ -0,0 +1,62 @@
+<!doctype html>
+<html lang="he">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+        <link href="css/rainbow.css" rel="stylesheet" type="text/css"/>
+        <title>landing-page-karin-2</title>
+    </head>
+    <body>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12 text-center mt-4">
+                    <img src="images/rainbow_logo.png" alt="rainbow logo" class="img-fluid">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-8 col-lg-6 text-right box mt-4" dir="rtl">
+                    <p class="tagline">לכבוד השנה החדשה,<br>
+                        +Rainbow מעניקה לכם שובר הנחה מיוחד</p>
+                    <form action="" method="POST" novalidate="novalidate" id="contact-form" class="mx-5 px-5">
+                        <div class="form-group px-sm-3">
+                            <label for="name" class="sr-only">שם מלא</label>
+                            <input type="text" class="form-control" id="name" name="name" placeholder="שם מלא" dir="rtl">
+                            <p class="text-danger text-center"></p>
+                        </div>
+                        <div class="form-group px-sm-3">
+                            <label for="phone" class="sr-only">טלפון נייד</label>
+                            <input type="tel" class="form-control" id="phone" name="phone" placeholder="טלפון נייד" dir="rtl">
+                            <p class="text-danger text-center"></p>
+                        </div>
+                        <div class="form-group px-sm-3">
+                            <label for="email" class="sr-only">אימייל</label>
+                            <input type="email" class="form-control" id="email" name="email" placeholder="אימייל" dir="rtl">
+                            <p class="text-danger text-center"></p>
+                        </div>                           
+
+                        <div class="px-3 text-center">
+                            <button type="submit" name="submit" id="submit" class="btn px-4" dir="rtl">שלח!</button>
+                            <p class="text-danger text-center"></p>
+                        </div>
+                    </form>
+                    <p class="text-right bottom-line mr-5 pr-5">
+                        בגדי החורף שלנו מחכים לחמם רק אתכם! <br> 
+                        רוצים לקבל קופון הנחה של <strong>30% על כל החנות?<br>
+                           </strong> רק השאירו פרטים- והשובר אצלכם במייל!
+                    </p>
+                </div>
+
+            </div>
+        </div>
+
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  
+        <script src="js/rainbow.js" type="text/javascript"></script>
+    </body>
+
+</html>
